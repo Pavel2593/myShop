5efbbdace19e27d4479e92e21efda828ebc19f97
Pavel2593 <shket97@yandex.ru>
2022-05-08 21:46:13 +0300

    Реализовано добавление изображений Реализована регистрация, авторизация и проверка по jwt.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,66 @@
 const ApiError = require('./../error/ApiError')
-class UserController {
-    async registration (request, response) {
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('./../models/models')
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        { id, email, role },
+        process.env.JWT_KEY,
+        { expiresIn: '24h' }
+    )
+}
+
+class UserController {
+    async registration (request, response, next) {
+        const {email, password, role} = request.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный логин или пароль'))
+        }
+
+        const candidate = await User.findOne({
+            where: {email}
+        })
+
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({
+            email,
+            password: hashPassword,
+            role,
+        })
+        const basket = await Basket.create({
+            userId: user.id
+        })
+        const token = generateJwt(user.id, user.email, user.role)
+
+        return response.json({token})
     }
 
-    async login (request, response) {
+    async login (request, response, next) {
+        const {email, password} = request.body
+        const user = await User.findOne({
+            where: {email}
+        })
+        
+        if (!user) {
+            return next(ApiError.badRequest('Email не верный'))
+        }
 
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.badRequest('Пароль не верный'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return response.json({token})
     }
 
     async check (request, response, next) {
-        const {id} = request.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан id'))
-        }
-        response.json(id)
+        const token = generateJwt(request.user.id, request.user.email, request.user.role)
+        return response.json({token})
     }
 }
 
