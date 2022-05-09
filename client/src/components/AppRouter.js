677fe8d0d17587afe9868d5f141184c59ae444fd
Pavel2593 <shket97@yandex.ru>
2022-05-09 13:32:09 +0300

    Сборка фронта

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Switch, Router, Redirect} from 'react-router-dom'
+console.log(Switch)
+const AppRouter = () => {
+  return (
+    <div>AppRouter</div>
+  )
+}
+
+export default AppRouter
